f880d0ceab83bbba399fe26e846e219f7d873468
Hasnain <hashamim@gmail.com>
2019-06-01 15:32:01 -0400

    removed activestorage association from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User < ApplicationRecord
     validates :email, :session_token, uniqueness: true
     validates :password, length: {minimum: 6, allow_nil: true}
 
-    has_one_attached :profile_pic
     #Sessions
     after_initialize :ensure_session_token
 
