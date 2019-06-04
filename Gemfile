5dad1eadeac656c70724ea25c341aef77a3ee85e
Hasnain <hashamim@gmail.com>
2019-06-04 10:23:53 -0400

    added seeds for heroku

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'jquery-rails'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
-
 gem "aws-sdk-s3"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+
+videos = {}
+videos[:stranger_things] = open("https://newtflix-dev.s3.amazonaws.com/Y481W8bavTg5uasHf6TzBRmD")
+videos[:house_of_cards] = open("https://newtflix-dev.s3.amazonaws.com/House+of+Cards++Official+Trailer+%5BHD%5D++Netflix.mp4")
+
+title_cards = {}
+title_cards[:stranger_things] = open("https://newtflix-dev.s3.amazonaws.com/cj981Fty7duAJwHZ95VPFGex")
+title_cards[:house_of_cards] = open("https://newtflix-dev.s3.amazonaws.com/house_of_cards.jpg")
+
+shows = []
+shows.push Show.create(title: "Stranger Things", year: 2017, maturity_rating: "TV-14", description: "When a young boy vanishes a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and a strange little girl")
+shows.push Show.create(title: "House of Cards", year: 2018, maturity_rating: "TV-MA", description: "With Frank out of the picture, Claire Underwood steps fully into her own as the first woman president but faces formidable threats to her legacy")
+
+genres = []
+genres.push Genre.create(name: "Horror")
+genres.push Genre.create(name: "Thriller")
+
+shows[0].genres << genres[0]
+shows[1].genres << genres[1]
+
+shows[0].video.attach(io: videos[:stranger_things], filename: "stranger_things.mp4")
+shows[0].title_card.attach(io: title_cards[:stranger_things], filename: "stranger_things.jpg")
+shows[1].video.attach(io: videos[:house_of_cards], filename: "house_of_cards.mp4")
+shows[1].title_card.attach(io: title_cards[:house_of_cards], filename: "house_of_cards.jpg")
+
+
+
