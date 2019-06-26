67f2b2883ebade10d089dcb1e9a2c0b5ccaccc96
Hasnain <hashamim@gmail.com>
2019-06-26 14:50:00 -0400

    refactored genrerow component to be class

--- frontend/components/browse/genre_row.jsx
+++ frontend/components/browse/genre_row.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import ShowRow from '../show_containers/show_row';
 import { Link } from 'react-router-dom';
 
-const GenreRow = (props) => {
-    return (
-        <>
-            <Link to={`/browse/genres/${props.id}`} className="genre-title">{props.name}<i className="fas fa-chevron-right"></i></Link> {/* Make Clickable */}
-            <ShowRow showsContained={props.showsContained} currentRow={props.currentGenre} setThisRow={props.setThisGenre} unsetThisRow={props.unsetThisGenre}/>
-        </>
-    )
+// const GenreRow = (props) => {
+//     return (
+//         <>
+//             <Link to={`/browse/genres/${props.id}`} className="genre-title">{props.name}<i className="fas fa-chevron-right"></i></Link> {/* Make Clickable */}
+//             <ShowRow showsContained={props.showsContained} currentRow={props.currentGenre} setThisRow={props.setThisGenre} unsetThisRow={props.unsetThisGenre}/>
+//         </>
+//     )
+// }
+class GenreRow extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <>
+                <Link to={`/browse/genres/${this.props.id}`} className="genre-title">{this.props.name}<i className="fas fa-chevron-right"></i></Link> {/* Make Clickable */}
+                <ShowRow showsContained={this.props.showsContained} currentRow={this.props.currentGenre} setThisRow={this.props.setThisGenre} unsetThisRow={this.props.unsetThisGenre} />
+            </>
+        )
+    }
 }
 export default GenreRow;
 
