932d4454f7ba3b65391a9cd0af452a5d4e8f0693
Hasnain <hashamim@gmail.com>
2019-06-26 14:49:33 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,11 @@ videos[:john_wick] = open("https://newtflix-dev.s3.amazonaws.com/John+Wick+(2014
 videos[:one_punch_man] = open("https://newtflix-dev.s3.amazonaws.com/One+Punch+Man+-+Official+Trailer.mp4")
 videos[:bojack] = open("https://newtflix-dev.s3.amazonaws.com/BoJack+Horseman++Official+Trailer+%5BHD%5D++Netflix.mp4")
 videos[:office] = open("https://newtflix-dev.s3.amazonaws.com/The+Office+TV+Series+2005+Season+1+Trailer.mp4")
-
+videos[:arrested] = open("https://newtflix-dev.s3.amazonaws.com/Arrested+Development+-+Season+4++Official+Trailer+%5BHD%5D++Netflix.mp4")
+videos[:it] = open("https://newtflix-dev.s3.amazonaws.com/IT+CHAPTER+TWO+-+Official+Teaser+Trailer+%5BHD%5D.mp4")
+videos[:toystory] = open("https://newtflix-dev.s3.amazonaws.com/Toy+Story+4++Official+Trailer.mp4")
+videos[:aladdin] = open("https://newtflix-dev.s3.amazonaws.com/%F0%9F%8E%A5+ALADDIN+(1992)++Full+Movie+Trailer+in+HD++1080p.mp4")
+videos[:friends] = open("")
 title_cards = {}
 title_cards[:stranger_things] = open("https://newtflix-dev.s3.amazonaws.com/cj981Fty7duAJwHZ95VPFGex")
 title_cards[:house_of_cards] = open("https://newtflix-dev.s3.amazonaws.com/house_of_cards.jpg")
@@ -33,6 +37,12 @@ title_cards[:john_wick] = open("https://newtflix-dev.s3.amazonaws.com/johnwick2-
 title_cards[:one_punch_man] = open("https://newtflix-dev.s3.amazonaws.com/one-punch-man.jpg")
 title_cards[:bojack] = open("https://newtflix-dev.s3.amazonaws.com/bojack-horseman-poster.jpg")
 title_cards[:office] = open("https://newtflix-dev.s3.amazonaws.com/the-office-poster.jpg")
+title_cards[:arrested] = open("https://newtflix-dev.s3.amazonaws.com/Watch-Arrested-Development-Online.jpg")
+title_cards[:it] = open("https://newtflix-dev.s3.amazonaws.com/it.jpg")
+title_cards[:toystory] = open("https://newtflix-dev.s3.amazonaws.com/toystory4.jpg")
+title_cards[:aladdin] = open("https://newtflix-dev.s3.amazonaws.com/aladdin.jpg")
+title_cards[:friends] = open("")
+
 
 stranger_things =  Show.create(title: "Stranger Things", year: 2017, maturity_rating: "TV-14", description: "When a young boy vanishes a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and a strange little girl")
 house_cards =  Show.create(title: "House of Cards", year: 2018, maturity_rating: "TV-MA", description: "With Frank out of the picture, Claire Underwood steps fully into her own as the first woman president but faces formidable threats to her legacy")
@@ -42,8 +52,10 @@ john_wick =  Show.create(title: "John Wick", year: 2014, maturity_rating: "R", d
 one_punch_man =  Show.create(title: "One Punch Man", year: 2019, maturity_rating: "TV-PG", description: "The story of Saitama, a hero that does it just for fun & can defeat his enemies with a single punch.")
 bojack =  Show.create(title: "Bojack Horseman", year: 2019, maturity_rating: "TV-MA", description: "BoJack Horseman was the star of the hit TV show \"Horsin' Around\" in the '90s, now he's washed up, living in Hollywood, complaining about everything, and wearing colorful sweaters.")
 office =  Show.create(title: "The Office", year: 2013, maturity_rating: "TV-PG", description: "A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.")
-friends = "FILL IN LATER"
-arrested = "FILL IN LATER"
+it_show =  Show.create(title: "It", year: 2017, maturity_rating: "R", description: "In the summer of 1989, a group of bullied kids band together to destroy a shape-shifting monster, which disguises itself as a clown and preys on the children of Derry, their small Maine town.")
+arrested = Show.create(title: "Arrested Development", year: 2018, maturity_rating: "TV-14", description: "Level-headed son Michael Bluth takes over family affairs after his father is imprisoned. But the rest of his spoiled, dysfunctional family are making his job unbearable.")
+aladdin = Show.create(title: "Aladdin", year: 1992, maturity_rating: "G", description: "A kindhearted street urchin and a power-hungry Grand Vizier vie for a magic lamp that has the power to make their deepest wishes come true.")
+toystory = Show.create(title: "Toy Story 4", year: 2019, maturity_rating: "G", description: "When a new toy called \"Forky\" joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy.")
 
 genres = Genre.create([{name: "Action"}, {name: "Comedy"},{name: "Horror"},{name: "Disney"},{name: "Thriller"},{name: "Animation"}])
 genres.push(Genre.create(name: "TV Shows"))
@@ -56,8 +68,24 @@ genres[0].shows << one_punch_man
 genres[1].shows << one_punch_man
 genres[1].shows << bojack
 genres[1].shows << office
-
 genres[1].shows << house_cards
+genres[1].shows << arrested
+
+genres[2].shows << it_show
+genres[2].shows << stranger_things
+
+genres[3].shows << avengers
+genres[3].shows << thor
+genres[3].shows << toystory
+genres[3].shows << aladdin
+
+genres[4].shows << john_wick
+genres[4].shows << house_cards
+
+genres[5].shows << one_punch_man
+genres[5].shows << toystory
+genres[5].shows << aladdin
+
 
 demo.shows << genres[0].shows
 demo.shows << genres[1].shows
@@ -78,3 +106,11 @@ office.video.attach(io: videos[:office], filename: "office.mp4")
 office.title_card.attach(io: title_cards[:office], filename: "office.jpg")
 bojack.video.attach(io: videos[:bojack], filename: "bojack.mp4")
 bojack.title_card.attach(io: title_cards[:bojack], filename: "bojack.jpg")
+it_show.video.attach(io: videos[:it], filename: "it_show.mp4")
+it_show.title_card.attach(io: title_cards[:it], filename: "it_show.jpg")
+arrested.video.attach(io: videos[:arrested], filename: "arrested.mp4")
+arrested.title_card.attach(io: title_cards[:arrested], filename: "arrested.jpg")
+toystory.video.attach(io: videos[:toystory], filename: "toystory.mp4")
+toystory.title_card.attach(io: title_cards[:toystory], filename: "toystory.jpg")
+aladdin.video.attach(io: videos[:aladdin], filename: "aladdin.mp4")
+aladdin.title_card.attach(io: title_cards[:aladdin], filename: "aladdin.jpg")
