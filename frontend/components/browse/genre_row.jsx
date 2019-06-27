ca044745c386d6ba819d53699a480fbd7fb35c59
Hasnain <hashamim@gmail.com>
2019-06-27 15:14:22 -0400

    basic carousel functionality

--- frontend/components/browse/genre_row.jsx
+++ frontend/components/browse/genre_row.jsx
@@ -22,15 +22,29 @@ class GenreRow extends React.Component{
             this.rows.push(rows.splice(0,5));
         }
     }
+
+    moveCarousel(num){
+        let ind = this.state.currentRow;
+        ind += num;
+        if(ind < 0){
+            ind = this.rows.length - 1;
+        } else if (ind >= this.rows.length){
+            ind = 0;
+        }
+        this.setState({
+            currentRow: ind,
+        })
+    }
+
     render(){
 
         return (
             <>
                 <Link to={`/browse/genres/${this.props.id}`} className="genre-title">{this.props.name}<i className="fas fa-chevron-right"></i></Link> {/* Make Clickable */}
                 <div className="genre-row">
-                    {/* <img className="left-carousel-btn" src={window.left_chevron_image}></img> */}
+                    <img className="left-carousel-btn" src={window.left_chevron_image} onClick={()=>this.moveCarousel(-1)}></img>
                     <ShowRow showsContained={this.rows[this.state.currentRow] ? this.rows[this.state.currentRow] : []} currentRow={this.props.currentGenre} setThisRow={this.props.setThisGenre} unsetThisRow={this.props.unsetThisGenre} />
-                    {/* <img className="right-carousel-btn" src={window.right_chevron_image}></img> */}
+                    <img className="right-carousel-btn" src={window.right_chevron_image} onClick={() => this.moveCarousel(1)}></img>
                 </div>
             </>
         )
